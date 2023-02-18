34cb489e83cd01ba4699da6830614b9cc4f2d231
Adrian Gierakowski <agierakowski@gmail.com>
2023-02-18 00:47:45 +0000

    use install mode for exe instead of chmod

--- modules/files.nix
+++ modules/files.nix
@@ -2,12 +2,7 @@
 let
   files     = config.file;
   fileType  = (import ./file-type.nix { inherit pkgs config lib; }).fileType;
-  chmod     = file:
-    if file.executable == null
-      then ""
-      else if file.executable
-        then "chmod +x $target"
-        else "chmod -x $target";
+  getMode   = file: if file.executable != null && file.executable then "755" else "644";
   git-add   = file:
     if file.git-add == null
       then ""
@@ -19,8 +14,7 @@ let
   # Execute this script to update the project's files
   copy-file = name: file: pkgs.writeShellScriptBin "${toName name}" ''
     target="$PRJ_ROOT${file.target}"
-    ${pkgs.coreutils}/bin/install -m 644 -D ${file.source} $target
-    ${chmod file}
+    ${pkgs.coreutils}/bin/install -m ${getMode file} -D ${file.source} $target
     ${git-add file}
   '';
   cmd.command     = builtins.concatStringsSep "\n" startups;
